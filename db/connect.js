dcd22863aebff7be11d6c8af1ef63337bed7cc3e
AKINTOLA STEPHEN IYANU <sakintola351@gmail.com>
2021-11-19 13:57:30 +0100

    Tea API first push

--- db/connect.js
+++ db/connect.js
@@ -3,7 +3,17 @@ require("dotenv")
 
 
 const connection = (url) => {
-  return mongoose.connect(process.env.MONGODB_CONNECTION_URI
+  return mongoose.connect(process.env.MONGODB_CONNECTION_URI, {
+    server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+    replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+  },
+    function (err) {
+      if (err) return console.log("Error: ", err);
+      console.log(
+        "MongoDB Connection -- Ready state is:",
+        mongoose.connection.readyState
+      );
+    }
   )
 }
 
